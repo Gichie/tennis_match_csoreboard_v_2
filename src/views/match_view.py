c59dcb1aabf23b37027d574a4f04371a97354298
Gichie <gichie@mail.ru>
2025-03-14 01:27:12 +0300

    add exeptions and logging

--- src/views/match_view.py
+++ src/views/match_view.py
@@ -1,6 +1,8 @@
 import os
+import logging
+from jinja2 import Environment, FileSystemLoader, TemplateNotFound
 
-from jinja2 import Environment, FileSystemLoader
+logger = logging.getLogger(__name__)
 
 
 class MatchView:
@@ -10,21 +12,36 @@ class MatchView:
 
         self.env = Environment(loader=loader, autoescape=True)
         self.env.filters['tennis_points'] = lambda x: {0: '0', 1: '15', 2: '30', 3: '40'}.get(x, '40')
-        self.env.filters['tie_break_points'] = lambda x: {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6',
-                                                          7: '7'}.get(x, f'{x}')
+        self.env.filters['tie_break_points'] = lambda x: f'{x}'
 
     def render_new_match_form(self, player1_name='', player2_name='', errors=None):
-        template = self.env.get_template('new_match.html')
-        return template.render(player1_name=player1_name, player2_name=player2_name, errors=errors or {})
+        try:
+            template = self.env.get_template('new_match.html')
+            return template.render(player1_name=player1_name, player2_name=player2_name, errors=errors or {})
+        except TemplateNotFound:
+            logger.error("Template not found: new_match.html")
+            return "Error rendering template"
 
     def render_match_score(self, context):
-        template = self.env.get_template('match_score.html')
-        return template.render(**context)  # Явное кодирование
+        try:
+            template = self.env.get_template('match_score.html')
+            return template.render(**context)  # Явное кодирование
+        except TemplateNotFound:
+            logger.error("Template not found: match_score.html")
+            return "Error rendering template"
 
     def render_final_score(self, context):
-        template = self.env.get_template('final_score.html')
-        return template.render(**context)
+        try:
+            template = self.env.get_template('final_score.html')
+            return template.render(**context)
+        except TemplateNotFound:
+            logger.error("Template not found: final_score.html")
+            return "Error rendering template"
 
     def render_error_page(self, context):
-        template = self.env.get_template('error.html')
-        return template.render(**context)
+        try:
+            template = self.env.get_template('error.html')
+            return template.render(**context)
+        except TemplateNotFound:
+            logger.error("Template not found: error.html")
+            return "Error rendering template"
